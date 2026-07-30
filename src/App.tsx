762dfecca61c8220e1ce28a0c8c803cd057012af
AudreyBurrell <audreyb2165@gmail.com>
2026-07-29 21:19:30 -0600

    Getting the progress to update and turn a different color depending on the value

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,106 @@
 import type { Task } from "./types/Task"; 
+import TaskItem from "./components/TaskItem";
 import "./styles/App.css";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+    const [tasks, setTasks] = useState<Task[]>([
+        {
+            id: 1,
+            title: "Finish TypeScript Todo App",
+            completed: false,
+            date: new Date(2026, 6, 29),
+            priority: "HIGH",
+        },
+        {
+            id: 2,
+            title: "Study React hooks",
+            completed: true,
+            date: new Date(2026, 6, 29),
+            priority: "MEDIUM",
+        },
+        {
+            id: 3,
+            title: "Read Frankenstein chapters 3-5",
+            completed: false,
+            date: new Date(2026, 6, 29),
+            priority: "LOW",
+        },
+        {
+            id: 4,
+            title: "Submit internship application",
+            completed: false,
+            date: new Date(2026, 6, 30),
+            priority: "HIGH",
+        },
+        {
+            id: 5,
+            title: "Go grocery shopping",
+            completed: false,
+            date: new Date(2026, 6, 30),
+            priority: "MEDIUM",
+        },
+        {
+            id: 6,
+            title: "Organize computer files",
+            completed: true,
+            date: new Date(2026, 6, 28),
+            priority: "LOW",
+        },
+        {
+            id: 7,
+            title: "Practice LeetCode problems",
+            completed: false,
+            date: new Date(2026, 6, 31),
+            priority: "HIGH",
+        },
+    ]);
+    useEffect(() => {
+        updateProgress();
+    }, [tasks]); 
+
+
     const [currentProgress, setProgress] = useState(0);
+
+
     let currentDate = new Date().toDateString();
+    const totalTasks = tasks.length;
+    
+    function updateProgress() {
+        let completeTasks = 0;
+        for (let task of tasks) {
+            if (task.completed) {
+                completeTasks++;
+            }
+        }
+        let newProgress = Math.round((completeTasks / totalTasks) * 100);
+        setProgress(newProgress);
+    }
+
+    function toggleTask(id: number) {
+        setTasks(
+            tasks.map((task) => {
+                if (task.id === id) {
+                    return {
+                        ...task,
+                        completed: !task.completed
+                    }
+                } else {
+                    return task;
+                }
+            })
+        )
+    } 
+
+    
 
     return (
         <main className="app">
-            {/* WHAT I WANT
-                Left hand side: 
-                    Title of application
-                    Date
-                    Filtering materials
-                    Progress for just that day
-                    Previous Date
-                    Tomorrow
-                    Add task
-                    Upload CSV
-                Right hand side:
-                    Where the tasks show up
-                    Delete button for the tasks
-                    Priority?
-                    
-            */}
             <div className="leftSideBar">
                 <h2>Todo List</h2>
                 <p id="date-display">{currentDate}</p>
-                <p id="progress-display">Progress: {currentProgress}%</p>
+                <p id="progress-display" className={currentProgress < 50 ? "regular-progress" : "green-progress"}>Progress: {currentProgress}%</p>
                 <div className="filterArea">
                     {/*When ready, filtering stuff goes here */}
                 </div>
@@ -44,6 +116,11 @@ function App() {
             <div className="rightSideBar">
                 <div className="toDoListArea">'
                     {/* to-do list appears here with title, delete button, and prioirty*/}
+                    <ul className="taskList">
+                        {tasks.map((task) => (
+                            <TaskItem key={task.id} task={task} onToggle={toggleTask} />
+                        ))}
+                    </ul>
                 </div>
             </div>
         
---
+++ src/components/TaskItem.tsx
@@ -0,0 +1,24 @@
+import type { Task } from "../types/Task";
+
+interface TaskItemProps {
+    task: Task;
+    onToggle: (id: number) => void;
+}
+
+function TaskItem({task, onToggle}: TaskItemProps) {
+    return (
+        <>
+            <li className={task.completed ? "task task_completed" : "task"}>
+                <span className="taskTitle">
+                    {task.title}
+                </span>
+
+                <button onClick={() => onToggle(task.id)}>
+                    {task.completed ? "↩ Undo" : "✓ Complete"}
+                </button>
+            </li>
+        </>
+    )
+} 
+
+export default TaskItem;
