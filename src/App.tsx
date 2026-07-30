a5f8695fcca8766e164d791dc179f1c47c18938d
AudreyBurrell <audreyb2165@gmail.com>
2026-07-29 19:42:47 -0600

    Making the html/css for my new app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import type { Task } from "./types/Task"; 
+import "./styles/App.css";
+
+import { useState } from "react";
 
 function App() {
+    let currentDate = new Date().toDateString();
+
     return (
-        <>
+        <main className="app">
             {/* WHAT I WANT
                 Left hand side: 
                     Title of application
@@ -19,8 +24,28 @@ function App() {
                     Priority?
                     
             */}
+            <div className="leftSideBar">
+                <h2>Todo List</h2>
+                <p id="date-display">{currentDate}</p>
+                <div className="filterArea">
+                    {/*When ready, filtering stuff goes here */}
+                </div>
+                <div className="navigation">
+                    <button>&#x276E; Previous</button>
+                    <button>Next &#x276F;</button>
+                </div>
+                <div className="taskAdditionArea">
+                    <button>+ Task</button>
+                    <button>Upload CSV</button>
+                </div>
+            </div>
+            <div className="rightSideBar">
+                <div className="toDoListArea">'
+                
+                </div>
+            </div>
         
-        </>
+        </main>
     )
 } 
 
