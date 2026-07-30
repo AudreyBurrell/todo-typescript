212729c70bbc51cf9e89c76cc8950360962c8276
AudreyBurrell <audreyb2165@gmail.com>
2026-07-29 19:32:15 -0600

    Making a plan for my app and creating the Task type

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import type { Task } from "./types/Task"; 
+
+function App() {
+    return (
+        <>
+            {/* WHAT I WANT
+                Left hand side: 
+                    Title of application
+                    Date
+                    Filtering materials
+                    Progress for just that day
+                    Previous Date
+                    Tomorrow
+                    Add task
+                    Upload CSV
+                Right hand side:
+                    Where the tasks show up
+                    Delete button for the tasks
+                    Priority?
+                    
+            */}
+        
+        </>
+    )
+} 
+
+export default App;
