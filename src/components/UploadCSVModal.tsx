70faa1000f6bbc13edc683c6b6a9cf77041dcc22
AudreyBurrell <audreyb2165@gmail.com>
2026-08-02 22:21:16 -0600

    Adding the html to the UploadCSVModal and the CSS for it

--- src/components/UploadCSVModal.tsx
+++ src/components/UploadCSVModal.tsx
@@ -1,4 +1,4 @@
-import "../styles/AddTaskModal.css";
+import "../styles/UploadCSVModal.css";
 import { useState } from "react";
 import type { Task } from "../types/Task"; 
 
@@ -7,10 +7,58 @@ interface uploadCSVModalProps {
 }  
 
 function UploadCSVModal({ onClose }: uploadCSVModalProps) {
-    return (
-        <div>
+    const [csvName, setCSVName] = useState("");
 
+    return (
+        <div className="modalOverlay">
+            <div className="modal">
+                <h2>Upload CSV</h2>
+                <div className="getTemplateArea">
+                    <a href="#">&#10515; Get Template</a>
+                </div>
+                <form onSubmit={(event) => {
+                    event.preventDefault();
+                    // onAddTask({
+                    //     id: Date.now(),
+                    //     title: title,
+                    //     completed: false,
+                    //     date: parseLocalDate(date),
+                    //     priority: priority
+                    // });
+                }}>
+                    <input
+                        id="csvFile"
+                        type="file"
+                        accept=".csv"
+                        style={{ display: "none" }}
+                        onChange={(event) => {
+                            const file = event.target.files?.[0];
+                            setCSVName(file ? file.name : "");
+                        }}
+                    />
+
+                    <label htmlFor="csvFile" className="chooseFileButton">
+                        Choose File
+                    </label>
+
+                    <p className="csvName">
+                        {csvName || "No file selected"}
+                    </p>
+                    <div className="modalButtons">
+                        <button type="submit">
+                            Add CSV
+                        </button>
+                        <button type="button" onClick={onClose}>
+                            Cancel
+                        </button>
+                    </div>  
+                </form>
+
+
+                
+            </div>
         </div>
+        
     )
 } 
 
