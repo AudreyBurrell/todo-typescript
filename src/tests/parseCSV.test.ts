bb2429ccd0308f61f8de5e8309eb06c58118bac0
AudreyBurrell <audreyb2165@gmail.com>
2026-08-03 18:54:13 +0000

    Adding some more tests

--- src/tests/parseCSV.test.ts
+++ src/tests/parseCSV.test.ts
@@ -99,4 +99,76 @@ describe("UploadCSVModal", () => {
             }, 
         ]);
     });
+    it("returns a valid task even if the completed status isn't all in lowercase", async () => {
+        const csvContent = 
+            `Instructions: Date format must be YYYY-MM-DD. Priority must be HIGH/MEDIUM/LOW. Completed must be true or false.
+
+            Title,Date,Priority,Completed
+            Testing Task, 2026-08-03, HIGH, FALSE
+            Testing Task 2, 2026-08-04, MEDIUM, True
+            Testing Task 3, 2026-08-05, LOW, fAlSe`;  
+        const file = new File([csvContent], "testTasks.csv", {
+            type: "text/csv"
+        });
+        const tasks = await uploadCSV(file);
+        expect(tasks).toEqual([
+            {
+                id: expect.any(Number),
+                title: "Testing Task",
+                date: new Date("2026-08-03"),
+                priority: "HIGH",
+                completed: false
+            },
+            {
+                id: expect.any(Number),
+                title: "Testing Task 2",
+                date: new Date("2026-08-04"),
+                priority: "MEDIUM",
+                completed: true
+            },
+            {
+                id: expect.any(Number),
+                title: "Testing Task 3",
+                date: new Date("2026-08-05"),
+                priority: "LOW",
+                completed: false
+            }
+        ]);
+    })
+    it("returns valid CSV data with mutliple tasks", async () => {
+        const csvContent = 
+            `Instructions: Date format must be YYYY-MM-DD. Priority must be HIGH/MEDIUM/LOW. Completed must be true or false.
+
+            Title,Date,Priority,Completed
+            Testing Task, 2026-08-03, HIGH, false
+            Testing Task 2, 2026-08-04, MEDIUM, true
+            Testing Task 3, 2026-08-05, LOW, false`;  
+        const file = new File([csvContent], "testTasks.csv", {
+            type: "text/csv"
+        });
+        const tasks = await uploadCSV(file);
+        expect(tasks).toEqual([
+            {
+                id: expect.any(Number),
+                title: "Testing Task",
+                date: new Date("2026-08-03"),
+                priority: "HIGH",
+                completed: false
+            },
+            {
+                id: expect.any(Number),
+                title: "Testing Task 2",
+                date: new Date("2026-08-04"),
+                priority: "MEDIUM",
+                completed: true
+            },
+            {
+                id: expect.any(Number),
+                title: "Testing Task 3",
+                date: new Date("2026-08-05"),
+                priority: "LOW",
+                completed: false
+            }
+        ]);
+    });
 });
